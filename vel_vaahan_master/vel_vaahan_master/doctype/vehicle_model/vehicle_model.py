ae2b80247da45321abc9ded2c0a6dd7a898614cb
KLA <smtwkla@gmail.com>
2024-11-14 14:37:28 +0530

    feat: tyre model, vehicle tyre specification, vehicle body type added;

--- vel_vaahan_master/vel_vaahan_master/doctype/vehicle_model/vehicle_model.py
+++ vel_vaahan_master/vel_vaahan_master/doctype/vehicle_model/vehicle_model.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2024, SMTW and contributors
 # For license information, please see license.txt
+from tabnanny import check
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class VehicleModel(Document):
-	pass
+	def before_save(self):
+		self.payload = self.gvw - self.uvw
+
+	def check_weights(self):
+		if self.gvw < self.uvw:
+			frappe.throw("Vehicle gross weight cannot be lesser than unladen weight.")
+
+	def validate(self):
+		self.check_weights()
