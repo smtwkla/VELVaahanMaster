068030585afb1f08c9dcda2dd165a08dc46727a6
KLA <smtwkla@gmail.com>
2024-11-17 00:07:49 +0530

    feat: passenger_vehicle_trip added

---
+++ vel_vaahan_master/vel_vaahan_master/doctype/passenger_vehicle_trip/passenger_vehicle_trip.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2024, SMTW and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Passenger Vehicle Trip", {
+	setup: function(frm) {
+		frm.set_query("driver", function() {
+			return {
+				filters: {
+					is_active: 1,
+				},
+			};
+		});
+		frm.set_query("vaahan", function() {
+			return {
+				query: "vel_vaahan_master.vel_vaahan_master.doctype.vehicle_model.vehicle_model.vehicle_query",
+				filters: {
+					is_in_use: 1,
+					is_gv: 0,
+				},
+			};
+		});
+	}
+});
