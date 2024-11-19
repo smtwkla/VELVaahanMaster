4c8bd3fe633ac71d387bd6a199a22e252a66147a
KLA <smtwkla@gmail.com>
2024-11-19 09:27:37 +0530

    fix: added patch for vehicle driver title migrate

---
+++ vel_vaahan_master/patches/v0.1.22/update_driver_title.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+def execute():
+	if frappe.db.has_column('Vehicle Driver', 'gen_title'):
+		frappe.db.sql("""
+			UPDATE `tabVehicle Driver` vd SET vd.title = vd.gen_title
+				WHERE vd.title is NULL;
+		""")
+		frappe.db.sql("""
+			ALTER TABLE `tabVehicle Driver` DROP COLUMN gen_title;
+		""")
