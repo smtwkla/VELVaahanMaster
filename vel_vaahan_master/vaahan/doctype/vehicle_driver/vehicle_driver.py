c6a2e25aa00f67b7da06fe606b6ea32fe49543e9
KLA <smtwkla@gmail.com>
2024-11-22 18:55:19 +0530

    fix: Driver License and photo fields added

--- vel_vaahan_master/vaahan/doctype/vehicle_driver/vehicle_driver.py
+++ vel_vaahan_master/vaahan/doctype/vehicle_driver/vehicle_driver.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2024, SMTW and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 from frappe.model.naming import getseries
-
+from frappe.utils.file_manager import get_file_name
 
 class VehicleDriver(Document):
 	def autoname(self):
@@ -12,3 +12,8 @@ class VehicleDriver(Document):
 		sl = getseries(prefix,4)
 		self.name = f'{prefix}{sl}'
 		self.title = f'{self.driver_name} [{self.name}]'
+
+	def before_save(self):
+		if self.attach_driver_image:
+			fn = get_file_name(self.attach_driver_image,"-s-")
+			print(fn, self.attach_driver_image)
